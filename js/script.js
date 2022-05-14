fd2724f3d4a1df691f006318c82ad8257f6d16c1
Vitalii Kniazev <13.kn.v.v@gmail.com>
2022-05-14 22:16:05 +0300

    add Personal Projects

--- js/script.js
+++ js/script.js
@@ -1,47 +1,50 @@
+const btnPrev = document.getElementById('carousel__btn_prev')
+const btnNext = document.getElementById('carousel__btn_next')
+const carousel = document.getElementById('carousel__img-inner')
+let carouselPosition = 0
+let step = 300
+let numberOfVisibleImg = 2
+const countImg = carousel.querySelectorAll('img').length
 
-/*
-//бургер
-const burger = document.querySelector('.header__burger');//бургеру присваеваем значение
-const menuBody = document.querySelector('.header__nav ');//меню выезжающее присваеваем значение
-const headerlang = document.querySelector('.header__lang ');
-if (burger) { //роверка есть ли бургер
-    burger.addEventListener("click", function (e) {//событие клика по бургеру
-        document.body.classList.toggle('lock');// к боди добавляем класс
-        burger.classList.toggle('change');//добавляем класс бургеру
-        menuBody.classList.toggle('_active');//добавляем меню выезжающему класс
-        headerlang.classList.toggle('show');
-    });
-}
-//бургер
+btnPrev.addEventListener('click', onPrev) 
+btnNext.addEventListener('click', onNext) 
 
-//прокрутка к секции
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]'); ////создаем массив ссылок у кого есть этот класс и атрибут data-goto (в html ссылкам пишем (data-goto="#home-id" это id блока куда перейти ) (class="menu__link" это класс для js))
-if (menuLinks.length > 0) {//// проверяем есть ли что-то в массиве
-    menuLinks.forEach(menuLink => {//// пробегаем по массиву
-        menuLink.addEventListener("click", onMenuLinkClick);//// вешаем событие клик и при клике направляемся в функцию onMenuLinkClick
-    });
-
-    function onMenuLinkClick(e) {//// создаем функцию onMenuLinkClick
-        const menuLink = e.target;//// получаем обьект по которому кликнули
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {//// условие  если (заполнен ли data-goto) и (существует ли обьект на который ссылается этот data-goto)
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);//// получаем в константу этот обьект
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;////вычисляем положение обьекта с учетом высоты шапки (gotoBlockValue = расстояние от верха + кол-во прокрученых пикселей по оси Y - высота шапки)
-                ////////////////закрываем выезжающее меню после нажатия ссылки///////////////
-            if (burger.classList.contains('change')) {/// если у массива burger есть класс .change
-                document.body.classList.remove('lock');// то убрать 
-                burger.classList.remove('change');//      эти классы
-                menuBody.classList.remove('_active');//   у этих
-                headerlang.classList.remove('show');//    массивах
-            }
-                /////////////////end закрыли выезжающее меню после нажатия ссылки////////////////////
-            window.scrollTo({//// обращаемся к окну браузера и функция прокрутки
-                top: gotoBlockValue,////указываю нужно прокрутиться к месту высчитанному до этого
-                behavior: "smooth"//// прокрутить плавно
-            });
-            e.preventDefault();////отключение работы ссылки
-        }
-    }
+function onPrev() {
+  carouselPosition += step
+  carouselPosition = Math.min(carouselPosition, 0)
+  carousel.style.marginLeft = carouselPosition + 'px'
 }
 
-//прокрутка к секции
-*/
+function onNext() {
+  carouselPosition -= step
+  carouselPosition = Math.max(carouselPosition, -step * (countImg - numberOfVisibleImg))
+  carousel.style.marginLeft = carouselPosition + 'px'
+}
+
+//////////////////////
+const imgArr = carousel.querySelectorAll('img')
+const moduleImgWrapp = document.querySelector('.module-imgWrapp')
+const moduleImg = document.querySelector('.module-img')
+const x = document.querySelector('.module-x')
+
+
+imgArr.forEach((img) => {
+  img.addEventListener('click', openModuleImg)
+})
+
+function openModuleImg(e) {
+  moduleImgWrapp.style.display = 'flex'
+  moduleImg.src = e.target.src
+  document.body.style.overflow  = 'hidden'
+}
+
+moduleImgWrapp.addEventListener('click', closeModuleImg)
+
+function closeModuleImg(e) {
+  if (e.target === moduleImgWrapp || e.target === x ) {
+    moduleImgWrapp.style.display = 'none'
+    moduleImg.src = ''
+    document.body.style.overflow  = 'visible'
+  }
+  
+}
